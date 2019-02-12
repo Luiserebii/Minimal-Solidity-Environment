0fac1c640817785d914661f9a3cd03a50c8e39ca
Luiserebii <luis@serebii.io>
2019-02-12 01:40:28 -0500

    Finished basic Calculator contract

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -5,5 +5,17 @@ contract Calculator {
     function add(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
+
+    function sub(uint256 a, uint256 b) public pure returns (uint256) {
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) public pure returns (uint256) {
+        return a / b;
+    }
     
 }
